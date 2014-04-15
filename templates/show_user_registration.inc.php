1b7eedef2e88e7c36da26b8947773ec0e1e4b3a9
Jean-François Bilger <changi67@gmail.com>
2014-04-16 00:05:51 +0200

    [MOD] Bootstrap user registration form step 2

--- templates/show_user_registration.inc.php
+++ templates/show_user_registration.inc.php
@@ -22,25 +22,16 @@
 
 $htmllang = str_replace("_","-",AmpConfig::get('lang'));
 $web_path = AmpConfig::get('web_path');
+$sTitle = AmpConfig::get('site_title').' - '.T_('Registration');
+$sHeadTitle = T_('Ampache Registration');
+$aCSS[] = AmpConfig::get('web_path').AmpConfig::get('theme_path').'/templates/default.css';
+$bBootstrap = true;
+require $prefix . '/templates/tiny_header.inc.php';
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $htmllang; ?>" lang="<?php echo $htmllang; ?>">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=<?php echo AmpConfig::get('site_charset'); ?>" />
-<title><?php echo AmpConfig::get('site_title'); ?> - <?php echo T_('Registration'); ?></title>
-<link rel="stylesheet" href="<?php echo AmpConfig::get('web_path'); ?><?php echo AmpConfig::get('theme_path'); ?>/templates/default.css" type="text/css" media="screen" />
-<link rel="shortcut icon" href="<?php echo AmpConfig::get('web_path'); ?>/favicon.ico" />
-</head>
-<body id="registerPage">
-<script src="<?php echo $web_path; ?>/modules/jquery/jquery.min.js" language="javascript" type="text/javascript"></script>
-<script src="<?php echo $web_path; ?>/lib/javascript/base.js" language="javascript" type="text/javascript"></script>
-<script src="<?php echo $web_path; ?>/lib/javascript/ajax.js" language="javascript" type="text/javascript"></script>
 
-<div id="maincontainer">
-<div id="header">
-<h1><?php echo scrub_out(AmpConfig::get('site_title')); ?></h1>
-<span><?php echo T_('Registration'); ?>...</span>
-</div>
+        <div class="jumbotron">
+            <h1><?php echo T_('Registration'); ?>...</h1>
+        </div>
 <?php
 
 $action = scrub_in($_REQUEST['action']);
@@ -49,8 +40,8 @@ $username = scrub_in($_REQUEST['username']);
 $email = scrub_in($_REQUEST['email']);
 $website = scrub_in($_REQUEST['website']);
 ?>
-<div id="registerbox">
-<form name="update_user" method="post" action="<?php echo $web_path; ?>/register.php" enctype="multipart/form-data">
+
+<form role="form" class="form-horizontal" name="update_user" method="post" action="<?php echo $web_path; ?>/register.php" enctype="multipart/form-data">
 <?php
 /*  If we should show the user agreement */
 if (AmpConfig::get('user_agreement')) { ?>
@@ -67,54 +58,65 @@ if (AmpConfig::get('user_agreement')) { ?>
 </div>
 <?php } // end if user_agreement ?>
 <h3><?php echo T_('User Information'); ?></h3>
-<div class="registerfield require">
-    <label for="username"><?php echo T_('Username'); ?>: <span class="asterix">*</span></label>
-    <input type='text' name='username' id='username' value='<?php echo scrub_out($username); ?>' />
-    <?php Error::display('username'); ?>
-    <?php Error::display('duplicate_user'); ?>
-</div>
-<div class="registerfield require">
-    <label for="fullname"><?php echo T_('Full Name'); ?>: <span class="asterix">*</span></label>
-    <input type='text' name='fullname' id='fullname' value='<?php echo scrub_out($fullname); ?>' />
-    <?php Error::display('fullname'); ?>
-</div>
-
-<div class="registerfield require">
-    <label for="email"><?php echo T_('E-mail'); ?>: <span class="asterix">*</span></label>
-    <input type='text' name='email' id='email' value='<?php echo scrub_out($email); ?>' />
-    <?php Error::display('email'); ?>
-</div>
-<div class="registerfield require">
-    <label for="email"><?php echo T_('Website'); ?>: <span class="asterix">*</span></label>
-    <input type='text' name='website' id='website' value='<?php echo scrub_out($website); ?>' />
-    <?php Error::display('website'); ?>
-</div>
-
-<div class="registerfield require">
-    <label for="password"><?php echo T_('Password'); ?>: <span class="asterix">*</span></label>
-    <input type='password' name='password_1' id='password_1' />
-    <?php Error::display('password'); ?>
-</div>
-
-<div class="registerfield require">
-    <label for="confirm_passord"><?php echo T_('Confirm Password'); ?>: <span class="asterix">*</span></label>
-    <input type='password' name='password_2' id='password_2' />
-</div>
-
-<div class="registerInformation">
-    <span><?php echo T_('* Required fields'); ?></span>
-</div>
+    <div class="form-group">
+        <label for="username" class="col-sm-3 control-label"><?php echo T_('Username'); ?>: <span class="asterix">*</span></label>
+        <div class="col-sm-4">
+            <input type='text' class="form-control" name='username' id='username' value='<?php echo scrub_out($username); ?>' />
+            <?php Error::display('username'); ?>
+            <?php Error::display('duplicate_user'); ?>
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="fullname" class="col-sm-3 control-label"><?php echo T_('Full Name'); ?>: <span class="asterix">*</span></label>
+        <div class="col-sm-4">
+            <input type='text' class="form-control" name='fullname' id='fullname' value='<?php echo scrub_out($fullname); ?>' />
+            <?php Error::display('fullname'); ?>
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="email" class="col-sm-3 control-label"><?php echo T_('E-mail'); ?>: <span class="asterix">*</span></label>
+        <div class="col-sm-4">
+            <input type='text' class="form-control" name='email' id='email' value='<?php echo scrub_out($email); ?>' />
+            <?php Error::display('email'); ?>
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="website" class="col-sm-3 control-label"><?php echo T_('Website'); ?>: <span class="asterix">*</span></label>
+        <div class="col-sm-4">
+            <input type='text' class="form-control" name='website' id='website' value='<?php echo scrub_out($website); ?>' />
+            <?php Error::display('website'); ?>
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="password_1" class="col-sm-3 control-label"><?php echo T_('Password'); ?>: <span class="asterix">*</span></label>
+        <div class="col-sm-4">
+            <input type='password' class="form-control" name='password_1' id='password_1' />
+            <?php Error::display('password'); ?>
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="password_2" class="col-sm-3 control-label"><?php echo T_('Confirm Password'); ?>: <span class="asterix">*</span></label>
+        <div class="col-sm-4">
+            <input type='password' class="form-control" name='password_2' id='password_2' />
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-sm-3">
+            <span><?php echo T_('* Required fields'); ?></span>
+        </div>
 
 <?php if (AmpConfig::get('captcha_public_reg')) { ?>
             <?php  echo captcha::form("&rarr;&nbsp;"); ?>
             <?php Error::display('captcha'); ?>
 <?php } ?>
 
-<div class="registerButtons">
-    <input type="hidden" name="action" value="add_user" />
-    <input type='submit' name='submit_registration' id='submit_registration' value='<?php echo T_('Register User'); ?>' />
-</div>
+        <div class="col-sm-4">
+            <input type="hidden" name="action" value="add_user" />
+            <button type='submit' class="btn btn-warning"><?php echo T_('Register User'); ?>'</button>
+        </div>
+    </div>
 </form>
+</div>
 <?php
 UI::show_footer();
 ?>
